b4c5770ad901e666625dc13a3ab1816e7f1edece
Wil Whipple <mrwhipple1.ceo@gmail.com>
2021-08-02 11:10:30 -0400

    dotnet update

---
+++ dotnet/Capstone/Controllers/IngredientController.cs
@@ -0,0 +1,22 @@
+﻿using Capstone.DAO.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class IngredientController : Controller
+    {
+        private readonly IUserDAO userDAO; 
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
