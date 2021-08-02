702f7564dc3fe2ca2aa8518ca9910944a921a4ce
Jose Vera <jvera409@gmail.com>
2021-08-02 16:54:26 -0400

    All New Changes To Server

--- dotnet/Capstone/DAO/IngredientsSqlDAO.cs
+++ dotnet/Capstone/DAO/IngredientsSqlDAO.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using Capstone.DAO.Interfaces;
+using Capstone.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Capstone.DAO
 {
-    public class IngredientSqlDAO
+    public class IngredientsSqlDAO : IIngredientsDAO
     {
+        private readonly string connectionString;
+
+        private string sqlAddIngredient = "INSERT INTO ingredients (ingredient_name, measurement_unit, ) " +
+            "VALUES ( @ingredient_name, @measurment_unit );";
+
+
+        public IngredientsSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public bool AddIngredient(Ingredients ingredients)
+        {
+            bool result = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmdAddIngredient = new SqlCommand(sqlAddIngredient, conn);
+
+                    cmdAddIngredient.Parameters.AddWithValue("@ingredient_name", ingredients.IngredientName);
+                    cmdAddIngredient.Parameters.AddWithValue("@measurment_unit", ingredients.MeasurementUnit);
+
+                    int count = cmdAddIngredient.ExecuteNonQuery();
+
+                    if (count > 0)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
     }
 }
