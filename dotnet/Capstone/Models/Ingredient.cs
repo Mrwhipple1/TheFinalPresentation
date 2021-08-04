28c899b6934eedb89b421f5ead9cd98d01d8dd9e
Jose Vera <jvera409@gmail.com>
2021-08-04 15:27:15 -0400

    Add and List

--- dotnet/Capstone/Models/Ingredient.cs
+++ dotnet/Capstone/Models/Ingredient.cs
@@ -9,7 +9,7 @@ namespace Capstone.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string IngredientName { get; set; }
 
         public int UserId { get; set; }
     }
